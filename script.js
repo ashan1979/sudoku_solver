bc0344737ecedf8414b6e655f09d055658781b6c
ashan <indrajee79@gmail.com>
2022-11-08 17:53:55 +0530

    api

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const submission = []
 for (let i = 0; i < squares; i++) {
     const inputElement = document.createElement('input')
     inputElement.setAttribute('type', 'number')
-    inputElement.setAttribute('min', '0')
+    inputElement.setAttribute('min', '1')
     inputElement.setAttribute('max', '9')
     puzzleBoard.appendChild(inputElement)
 }
@@ -21,6 +21,30 @@ const joinValues = () => {
         }
     })
     console.log(submission)
+}
+
+const solve = async () => {
+    joinValues()
+  const data = { numbers: submission.join('') }
+
+  fetch('http://localhost:8000/solve', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data)
+    populateValues(data.solvable, data.solution)
+    submission = []
+  })
+  .catch((error) => {
+    console.error('Error:', error)
+  })
+
 }
 
 solveButton.addEventListener('click', joinValues)
